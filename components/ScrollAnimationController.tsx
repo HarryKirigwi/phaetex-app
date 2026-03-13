e4b9c6f66b341fb2544bcd6b3c43e85d82cfcf1c
Harrison <harrykigs2003@gmail.com>
2026-03-13 07:50:34 +0300

    Fixed the not loading home section

--- components/ScrollAnimationController.tsx
+++ components/ScrollAnimationController.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { useEffect } from "react";
+import { usePathname } from "next/navigation";
 
 const ROOT_MARGIN = "0px 0px -80px 0px";
 const THRESHOLD = 0.1;
 
 export default function ScrollAnimationController() {
+  const pathname = usePathname();
+
   useEffect(() => {
     const els = document.querySelectorAll("[data-animate]");
     if (els.length === 0) return;
@@ -23,7 +26,7 @@ export default function ScrollAnimationController() {
 
     els.forEach((el) => observer.observe(el));
     return () => els.forEach((el) => observer.unobserve(el));
-  }, []);
+  }, [pathname]);
 
   return null;
 }
